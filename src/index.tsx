44609f1faa329738c2b2ffaa93b3b24f87039e80
SuperWallaby <colton950901@gmail.com>
2020-04-25 13:47:38 +0900

    예약페이지 빌드 고뇌중

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App, { APP_PROP } from "./App";
-import * as serviceWorker from "./serviceWorker";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App, { APP_PROP } from './App';
+import * as serviceWorker from './serviceWorker';
 export interface RESV_INIT_OPTION {
-  lang?: "kr" | "en";
+	lang?: 'kr' | 'en';
 }
 
 class JD_RESV implements APP_PROP {
-  publickey: string;
-  initOp?: RESV_INIT_OPTION;
+	publickey: string;
+	initOp?: RESV_INIT_OPTION;
 
-  constructor(pbk: string, options?: RESV_INIT_OPTION) {
-    this.publickey = pbk;
-    this.initOp = options;
-  }
+	constructor(pbk: string, options?: RESV_INIT_OPTION) {
+		this.publickey = pbk;
+		this.initOp = options;
+	}
 
-  start() {
-    ReactDOM.render(
-      <App publickey={this.publickey} {...this.initOp} />,
-      document.getElementById("JD_RESV_PAGE")
-    );
-  }
+	start() {
+		ReactDOM.render(<App publickey={this.publickey} {...this.initOp} />, document.getElementById('JD_RESV_PAGE'));
+	}
 }
 
-const TEST_PUBLICK_KEY = "163105a1-6104-36d5-8383-7d3a0320bd39";
+const TEST_PUBLICK_KEY = '163105a1-6104-36d5-8383-7d3a0320bd39';
 
-if (process.env.NODE_ENV === "development") {
-  const jdmoudle = new JD_RESV(TEST_PUBLICK_KEY);
-  jdmoudle.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+// 	const jdmoudle = new JD_RESV(TEST_PUBLICK_KEY);
+// 	jdmoudle.start();
+// }
+
+// @ts-ignore
+window.JDresv = JD_RESV;
+
+// @ts-ignore
+const aa = new window.JDresv(TEST_PUBLICK_KEY);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
